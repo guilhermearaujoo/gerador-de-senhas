17a68ef818f998a192f0324c48ac8227b65511a4
guilhermearaujoo <ghma.em1@gmail.com>
2022-11-08 19:51:11 -0300

    adiciona opção de colar do teclado

--- src/main.js
+++ src/main.js
@@ -1,5 +1,7 @@
 import { nanoid } from 'nanoid'
-import './style.css'
+import copy from 'clipboard-copy';
+import './style.css';
+
 
 const button = document.querySelector('button');
 const h2 = document.querySelector('h2');
@@ -7,4 +9,10 @@ const h2 = document.querySelector('h2');
 
 button.addEventListener('click', () => {
   h2.innerHTML = nanoid();
+});
+
+
+h2.addEventListener('click', (event) => {
+  copy(event.target.innerHTML);
+  alert('Senha copiada!');
 });
